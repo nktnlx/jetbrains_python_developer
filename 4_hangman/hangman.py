9ad4b2c716f1ec5e28d22f6d0d580f8cb92f176c
nktnlx <nktn.lx@gmail.com>
2020-11-20 17:09:38 +0300

    hangman stage 7

--- 4_hangman/hangman.py
+++ 4_hangman/hangman.py
@@ -103,6 +103,62 @@
 
 
 # STAGE 6
+# import random
+# import sys
+#
+# words_to_guess = ['python', 'java', 'kotlin', 'javascript']
+#
+# print(*'HANGMAN')
+#
+#
+# def guess(word):
+#     tries = 8
+#     letters = []
+#     index = []
+#
+#     while tries > 0:
+#         print('')
+#         for letter in letters:
+#             start = 0
+#             counter = 0
+#             while counter < word.count(letter):
+#                 index.append(word.find(letter, start))
+#                 start = word.find(letter, start) + 1
+#                 counter += 1
+#
+#         for j in range(len(word)):
+#             if j in index:
+#                 print(word[j], end='')
+#             else:
+#                 print('-', end='')
+#
+#
+#         if len(set(index)) == len(word):
+#             print('')
+#             print('''You guessed the word!
+# You survived!''')
+#             sys.exit(1)
+#
+#         print('')
+#         user_letter = (input('Input a letter: '))
+#         if user_letter not in word:
+#             print('That letter doesn\'t appear in the word')
+#             tries -= 1
+#             continue
+#         elif user_letter in letters:
+#             print('No improvements')
+#             tries -= 1
+#             continue
+#         elif user_letter in word:
+#             letters.append(user_letter)
+#
+#     print('You lost!')
+
+
+# guess(random.choice(words_to_guess))
+
+
+# STAGE 7
 import random
 import sys
 
@@ -115,9 +171,9 @@ def guess(word):
     tries = 8
     letters = []
     index = []
+    wrong_letters = []
 
     while tries > 0:
-        print('')
         for letter in letters:
             start = 0
             counter = 0
@@ -126,39 +182,48 @@ def guess(word):
                 start = word.find(letter, start) + 1
                 counter += 1
 
+        if len(set(index)) == len(word):
+            print(f'''You guessed the word {word}!
+You survived!''')
+            sys.exit(1)
+
+        print('')
+
         for j in range(len(word)):
             if j in index:
                 print(word[j], end='')
             else:
                 print('-', end='')
 
-
-        if len(set(index)) == len(word):
-            print('')
-            print('''You guessed the word!
-You survived!''')
-            sys.exit(1)
-
         print('')
         user_letter = (input('Input a letter: '))
-        if user_letter not in word:
+
+        if len(user_letter) != 1:
+            print("You should input a single letter")
+            continue
+
+        elif user_letter.isascii() == False or user_letter.islower() == False:
+            print("Please enter a lowercase English letter")
+            continue
+
+        elif user_letter in letters or user_letter in wrong_letters:
+            print('You\'ve already guessed this letter')
+            continue
+
+        elif user_letter not in word:
             print('That letter doesn\'t appear in the word')
+            wrong_letters.append(user_letter)
             tries -= 1
             continue
-        elif user_letter in letters:
-            print('No improvements')
-            tries -= 1
-            continue
+
         elif user_letter in word:
             letters.append(user_letter)
 
     print('You lost!')
 
 
+#guess(words_to_guess[3])
 guess(random.choice(words_to_guess))
-
-
-
 
 
 
