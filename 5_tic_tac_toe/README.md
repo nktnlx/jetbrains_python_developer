62e4e004d9c2fe9aad58545130ab3299cb883975
nktnlx <nktn.lx@gmail.com>
2020-11-23 19:48:00 +0300

    tic-tac-toe stage5_final

--- 5_tic_tac_toe/README.md
+++ 5_tic_tac_toe/README.md
@@ -3,6 +3,71 @@ This is the forth project completed by me from [JetBrains Python Developer track
 Tic-Tac-Toe -- is a popular pen and pencil game, also known as Xs and Os. This version of the game is for two players. Players set 'X' or 'O' one after another until one of the players have a straight line (including diagonals). If all cells in a 3x3 field are occupied and no one got a straigft line - it is a tie!  
 
 
+Gameplay example:  
+```
+---------
+| _ _ _ |
+| _ _ _ |
+| _ _ _ |
+---------
+Enter the coordinates: 1 3
+---------
+| X _ _ |
+| _ _ _ |
+| _ _ _ |
+---------
+Enter the coordinates: 2 2
+---------
+| X _ _ |
+| _ O _ |
+| _ _ _ |
+---------
+Enter the coordinates: 3 3
+---------
+| X _ X |
+| _ O _ |
+| _ _ _ |
+---------
+Enter the coordinates: 2 3
+---------
+| X O X |
+| _ O _ |
+| _ _ _ |
+---------
+Enter the coordinates: 1 2
+---------
+| X O X |
+| X O _ |
+| _ _ _ |
+---------
+Enter the coordinates: 1 1
+---------
+| X O X |
+| X O _ |
+| O _ _ |
+---------
+Enter the coordinates: 2 1
+---------
+| X O X |
+| X O _ |
+| O X _ |
+---------
+Enter the coordinates: 3 1
+---------
+| X O X |
+| X O _ |
+| O X O |
+---------
+Enter the coordinates: 3 2
+---------
+| X O X |
+| X O X |
+| O X O |
+---------
+Draw
+
+```  
+
 
 The code was created using Python 3.8  
 If your machine has an installed Python 3, you can run the code in terminal(command line):  
--- 5_tic_tac_toe/tictactoe.py
+++ 5_tic_tac_toe/tictactoe.py
@@ -1,3 +1,165 @@
+## STAGE 5 (final)
+import sys
+import string
+
+user_inp = '_' * 9
+
+
+# defining the game field based on user input
+def field(inp):
+    matrix = []
+    matrix.append('| ' + inp[0] + ' ' + inp[1] + ' ' + inp[2] + ' |')  # creating line 2
+    width = len(matrix[0])  # width of game field
+    matrix.append('-' * width)  # creating line 1
+    matrix.append('| ' + inp[3] + ' ' + inp[4] + ' ' + inp[5] + ' |')  # creating line 3
+    matrix.append('| ' + inp[6] + ' ' + inp[7] + ' ' + inp[8] + ' |')  # creating line 4
+    matrix.append('-' * width)  # creating line 5
+    matrix[0], matrix[1] = matrix[1], matrix[0]  # swapping line 1 and line 2
+    return matrix
+
+
+# a dictionary that matches indexes with coordinates of each cell on the game field:
+# suppose the bottom left cell has the coordinates (1, 1)
+# and the top right cell has the coordinates (3, 3)
+coord_dict = {0: 13,
+              1: 23,
+              2: 33,
+              3: 12,
+              4: 22,
+              5: 32,
+              6: 11,
+              7: 21,
+              8: 31}
+
+
+# visualizing the game
+def render(board):
+    for item in board:
+        print(item)
+
+
+# finding empty cells on the game field
+def empty(inp):
+    empty_cells = []
+    for ind, val in enumerate(inp):
+        if val == '_':
+            empty_cells.append(ind)
+    return empty_cells
+
+
+# entering coordinates of the next move
+def enter_coord(cells):
+    while True:
+        user = input('Enter the coordinates: ')
+
+        if user.replace(' ', '')[0] not in string.digits or user.replace(' ', '')[1] not in string.digits:
+            print('You should enter numbers!')
+            continue
+        elif int(user[0]) > 3 or int(user[2]) > 3:
+            print('Coordinates should be from 1 to 3!')
+            continue
+
+        user_choice = int(user.replace(' ', ''))
+
+        # switching from string index to coordinates of empty cells
+        empty_coords = []
+        for ind in cells:
+            empty_coords.append(coord_dict[ind])
+
+        if user_choice not in empty_coords:
+            print('This cell is occupied! Choose another one!')
+            continue
+        else:
+            for k, v in coord_dict.items():  # converting coordinate back to string index
+                if v == user_choice:
+                    user_choice_ind = k
+
+        return user_choice_ind
+
+
+# prints the 'X' symbol due to entered coordinates of the user
+def next_move(inp, user_choice, move):
+    lst = list(inp)
+    if move % 2 == 0:
+        lst[user_choice] = 'X'
+    else:
+        lst[user_choice] = 'O'
+    inp = ''.join(lst)
+    return inp
+
+
+# finding the game state
+def game_states(state):
+    dia_1 = state[1][2] + state[2][4] + state[3][6]  # creating diagonal TopLeft - RightBottom
+    dia_2 = state[1][6] + state[2][4] + state[3][2]  # creating diagonal TopRight - BottomLeft
+    line_1 = state[1][2] + state[1][4] + state[1][6] # creating winning lines
+    line_2 = state[2][2] + state[2][4] + state[2][6] # creating winning lines
+    line_3 = state[3][2] + state[3][4] + state[3][6] # creating winning lines
+    col_1, col_2, col_3 = ['' for _ in range(3)]
+
+    for el in state[1:4]:
+        col_1 += el[2]  # creating leftmost column
+        col_2 += el[4]  # creating central column
+        col_3 += el[6]  # creating rightmost column
+
+    # 'X' winning options list
+    x_win = [dia_1.count('X') == 3,
+             dia_2.count('X') == 3,
+             col_1.count('X') == 3,
+             col_2.count('X') == 3,
+             col_3.count('X') == 3,
+             line_1.count('X') == 3,
+             line_2.count('X') == 3,
+             line_3.count('X') == 3]
+
+    # 'O' winning options list
+    o_win = [dia_1.count('O') == 3,
+             dia_2.count('O') == 3,
+             col_1.count('O') == 3,
+             col_2.count('O') == 3,
+             col_3.count('O') == 3,
+             line_1.count('O') == 3,
+             line_2.count('O') == 3,
+             line_3.count('O') == 3]
+
+    if any(x_win):
+        return 'X wins'
+    if any(o_win):
+        return 'O wins'
+
+    # counting empty fields left
+    underscore_count = 0
+    for el in state[1:4]:
+        underscore_count += el.count('_')
+
+    if underscore_count == 0:
+        return 'Draw'
+    else:
+        return 'Game in progress'
+
+
+def main(user_input):
+    game = field(user_input)
+    render(game)
+    status = game_states(game)
+    move = 0  # helps to alternate X and O moves (one after another)
+    while status == 'Game in progress':
+        new_cells = empty(user_input)
+        user_ind = enter_coord(new_cells)
+        next_field = next_move(user_input, user_ind, move)
+        move += 1
+        game = field(next_field)
+        render(game)
+        user_input = next_field
+        status = game_states(game)
+
+    print(game_states(game))
+
+
+if __name__ == '__main__':
+    main(user_inp)
+
+
 # STAGE 1
 # printing a random game result
 # import random
@@ -84,93 +246,92 @@
 # print(game_states(game))  # returning the game state
 
 
-
 # STAGE 4
-import string
+# import string
 
-user_inp = input('Enter cells: ')
-
-
-# defining the game field based on user input
-def field(inp):
-    matrix = []
-    matrix.append('| ' + inp[0] + ' ' + inp[1] + ' ' + inp[2] + ' |')  # creating line 2
-    width = len(matrix[0])  # width of game field
-    matrix.append('-' * width)  # creating line 1
-    matrix.append('| ' + inp[3] + ' ' + inp[4] + ' ' + inp[5] + ' |')  # creating line 3
-    matrix.append('| ' + inp[6] + ' ' + inp[7] + ' ' + inp[8] + ' |')  # creating line 4
-    matrix.append('-' * width)  # creating line 5
-    matrix[0], matrix[1] = matrix[1], matrix[0]  # swapping line 1 and line 2
-    return matrix
-
-
-# a dictionary that matches indexes with coordinates of each cell on the game field:
-# suppose the bottom left cell has the coordinates (1, 1)
-# and the top right cell has the coordinates (3, 3)
-coord_dict = {0: 13,
-              1: 23,
-              2: 33,
-              3: 12,
-              4: 22,
-              5: 32,
-              6: 11,
-              7: 21,
-              8: 31}
-
-
-# finding empty cells on the game field
-def empty(inp):
-    empty_cells = []
-    for ind, val in enumerate(user_inp):
-        if val == '_':
-            empty_cells.append(ind)
-    return empty_cells
-
-
-# entering coordinates of the next move
-def enter_coord(cells):
-    while True:
-        user = input('Enter the coordinates: ')
-
-        if user.replace(' ', '')[0] not in string.digits or user.replace(' ', '')[1] not in string.digits:
-            print('You should enter numbers!')
-            continue
-        elif int(user[0]) > 3 or int(user[2]) > 3:
-            print('Coordinates should be from 1 to 3!')
-            continue
-
-        user_choice = int(user.replace(' ', ''))
-
-        # switching from string index to coordinates of empty cells
-        empty_coords = []
-        for ind in cells:
-            empty_coords.append(coord_dict[ind])
-
-        if user_choice not in empty_coords:
-            print('This cell is occupied! Choose another one!')
-            continue
-        else:
-            for k, v in coord_dict.items():  # converting coordinate back to string index
-                if v == user_choice:
-                    user_choice_ind = k
-
-        return user_choice_ind
-
-
-# prints the 'X' symbol due to entered coordinates of the user
-def next_move(inp, user_choice):
-    lst = list(inp)
-    lst[user_choice] = 'X'
-    inp = ''.join(lst)
-    return inp
-
-
-game = field(user_inp)
-[print(item) for item in game]  # visualizing the game
-
-new_cells = empty(user_inp)
-user_ind = enter_coord(new_cells)
-next_field = next_move(user_inp, user_ind)
-
-game = field(next_field)
-[print(item) for item in game]  # visualizing the game after users input
+# user_inp = input('Enter cells: ')
+#
+#
+# # defining the game field based on user input
+# def field(inp):
+#     matrix = []
+#     matrix.append('| ' + inp[0] + ' ' + inp[1] + ' ' + inp[2] + ' |')  # creating line 2
+#     width = len(matrix[0])  # width of game field
+#     matrix.append('-' * width)  # creating line 1
+#     matrix.append('| ' + inp[3] + ' ' + inp[4] + ' ' + inp[5] + ' |')  # creating line 3
+#     matrix.append('| ' + inp[6] + ' ' + inp[7] + ' ' + inp[8] + ' |')  # creating line 4
+#     matrix.append('-' * width)  # creating line 5
+#     matrix[0], matrix[1] = matrix[1], matrix[0]  # swapping line 1 and line 2
+#     return matrix
+#
+#
+# # a dictionary that matches indexes with coordinates of each cell on the game field:
+# # suppose the bottom left cell has the coordinates (1, 1)
+# # and the top right cell has the coordinates (3, 3)
+# coord_dict = {0: 13,
+#               1: 23,
+#               2: 33,
+#               3: 12,
+#               4: 22,
+#               5: 32,
+#               6: 11,
+#               7: 21,
+#               8: 31}
+#
+#
+# # finding empty cells on the game field
+# def empty(inp):
+#     empty_cells = []
+#     for ind, val in enumerate(inp):
+#         if val == '_':
+#             empty_cells.append(ind)
+#     return empty_cells
+#
+#
+# # entering coordinates of the next move
+# def enter_coord(cells):
+#     while True:
+#         user = input('Enter the coordinates: ')
+#
+#         if user.replace(' ', '')[0] not in string.digits or user.replace(' ', '')[1] not in string.digits:
+#             print('You should enter numbers!')
+#             continue
+#         elif int(user[0]) > 3 or int(user[2]) > 3:
+#             print('Coordinates should be from 1 to 3!')
+#             continue
+#
+#         user_choice = int(user.replace(' ', ''))
+#
+#         # switching from string index to coordinates of empty cells
+#         empty_coords = []
+#         for ind in cells:
+#             empty_coords.append(coord_dict[ind])
+#
+#         if user_choice not in empty_coords:
+#             print('This cell is occupied! Choose another one!')
+#             continue
+#         else:
+#             for k, v in coord_dict.items():  # converting coordinate back to string index
+#                 if v == user_choice:
+#                     user_choice_ind = k
+#
+#         return user_choice_ind
+#
+#
+# # prints the 'X' symbol due to entered coordinates of the user
+# def next_move(inp, user_choice):
+#     lst = list(inp)
+#     lst[user_choice] = 'X'
+#     inp = ''.join(lst)
+#     return inp
+#
+#
+# game = field(user_inp)
+# [print(item) for item in game]  # visualizing the game
+#
+# new_cells = empty(user_inp)
+# user_ind = enter_coord(new_cells)
+# next_field = next_move(user_inp, user_ind)
+#
+# game = field(next_field)
+# [print(item) for item in game]  # visualizing the game after users input
