cacb44f61be51e16f8d13f75516a070a112796e0
nktnlx <nktn.lx@gmail.com>
2020-12-13 10:49:46 +0300

    to-do list stage 1

--- 10_to_do_list/todolist.py
+++ 10_to_do_list/todolist.py
@@ -1,1 +1,18 @@
 # STAGE 1
+class TaskList():
+    def __init__(self, lst):
+        self.lst = lst
+
+    def today(self):
+        print('Today:')
+        i = 0
+        for item in self.lst:
+            i += 1
+            print(f'{i}) {item}')
+
+
+tasks = ['Do yoga', 'Make breakfast', 'Learn basics of SQL', 'Learn what is ORM']
+
+today = TaskList(tasks)
+
+today.today()
