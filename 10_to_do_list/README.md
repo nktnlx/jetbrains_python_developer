16ff8dbafcedeca92b18f242e9ba6b89dc6f7ed4
nktnlx <nktn.lx@gmail.com>
2020-12-14 18:51:37 +0300

    to-do list stage 3 (OOP way)

--- 10_to_do_list/README.md
+++ 10_to_do_list/README.md
@@ -5,52 +5,138 @@ To-Do List -- To-Do list that will help you manage your tasks. Practiced loops, 
 An example of the script execution:  
 ```
 1) Today's tasks
-2) Add task
+2) Week's tasks
+3) All tasks
+4) Add task
 0) Exit
 1
 
-Today:
+Today 14 Dec:
 Nothing to do!
 
 1) Today's tasks
-2) Add task
+2) Week's tasks
+3) All tasks
+4) Add task
 0) Exit
-2
+4
 
 Enter task
-Learn sqlalchemy!  
+Learn Python
+Enter deadline
+2020-12-18
 The task has been added!
 
 1) Today's tasks
-2) Add task
+2) Week's tasks
+3) All tasks
+4) Add task
 0) Exit
-2
+4
 
 Enter task
-Workout
+Finish stage 3
+Enter deadline
+2020-12-14
 The task has been added!
 
 1) Today's tasks
-2) Add task
+2) Week's tasks
+3) All tasks
+4) Add task
 0) Exit
-2
+4
 
 Enter task
-Read a book
+Start stage 4
+Enter deadline
+2020-12-14
 The task has been added!
 
 1) Today's tasks
-2) Add task
+2) Week's tasks
+3) All tasks
+4) Add task
+0) Exit
+4
+
+Enter task
+Repeat SQL syntax
+Enter deadline
+2020-12-19
+The task has been added!
+
+1) Today's tasks
+2) Week's tasks
+3) All tasks
+4) Add task
+0) Exit
+4
+
+Enter task
+Celebrate New Year!
+Enter deadline
+2021-01-01
+The task has been added!
+
+1) Today's tasks
+2) Week's tasks
+3) All tasks
+4) Add task
 0) Exit
 1
 
-Today:
-1. Learn sqlalchemy!
-2. Workout
-3. Read a book
+Today 14 Dec:
+1. Finish stage 3
+2. Start stage 4
 
 1) Today's tasks
-2) Add task
+2) Week's tasks
+3) All tasks
+4) Add task
+0) Exit
+2
+
+Monday 14 Dec:
+1. Finish stage 3
+2. Start stage 4
+
+Tuesday 15 Dec:
+Nothing to do!
+
+Wednesday 16 Dec:
+Nothing to do!
+
+Thursday 17 Dec:
+Nothing to do!
+
+Friday 18 Dec:
+1. Learn Python
+
+Saturday 19 Dec:
+1. Repeat SQL syntax
+
+Sunday 20 Dec:
+Nothing to do!
+
+1) Today's tasks
+2) Week's tasks
+3) All tasks
+4) Add task
+0) Exit
+3
+
+All tasks:
+1. Finish stage 3. 14 Dec
+2. Start stage 4. 14 Dec
+3. Learn Python. 18 Dec
+4. Repeat SQL syntax. 19 Dec
+5. Celebrate New Year!. 1 Jan
+
+1) Today's tasks
+2) Week's tasks
+3) All tasks
+4) Add task
 0) Exit
 0
 
--- 10_to_do_list/todolist.py
+++ 10_to_do_list/todolist.py
@@ -24,41 +24,41 @@
 # from sqlalchemy import Column, Integer, String, Date
 # from sqlalchemy.orm import sessionmaker
 # from datetime import datetime
-
+#
 # # creating DB file
 # engine = create_engine('sqlite:///todo.db?check_same_thread=False')
-
+#
 # # creating declarative meta class from which our Tasks class will inherit
 # Base = declarative_base()
-
-
+#
+#
 # # creating definition of the table in our Task class (child class to the Base)
 # class Task(Base):
 #     __tablename__ = 'task'
 #     id = Column(Integer, primary_key=True)
 #     task = Column(String, default='')
 #     deadline = Column(Date, default=datetime.today)
-
+#
 #     def __repr__(self):
 #         return self.task
-
-
+#
+#
 # # create a table in our DB
 # Base.metadata.create_all(engine)
-
+#
 # # creating a session object to access our database
 # Session = sessionmaker(bind=engine)
 # session = Session()
-
-
+#
+#
 # # tasker main menu
 # def menu():
 #     choice = input('''1) Today's tasks
 # 2) Add task
 # 0) Exit\n''')
 #     return choice
-
-
+#
+#
 # # checking what's on today
 # def today_tasks():
 #     print('\nToday:')
@@ -69,8 +69,8 @@
 #         for item in rows:
 #             print(f'{item.id}. {item.task}')
 #         print('')
-
-
+#
+#
 # # adding a task for today to our DB
 # def add_task():
 #     new_task = input('Enter task\n')
@@ -79,8 +79,8 @@
 #     session.commit()
 #     print('The task has been added!\n')
 #     # rows = session.query(Task).all()
-
-
+#
+#
 # # running the script
 # def main():
 #     while True:
@@ -92,23 +92,23 @@
 #         elif user_choice == '0':
 #             print('\nBye!')
 #             break
-
-
+#
+#
 # main()
 
 
-
-# STAGE 2 (OOP way)
+# STAGE 3
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime
+from sqlalchemy import and_
+from datetime import datetime, timedelta
 
 # creating DB file
 engine = create_engine('sqlite:///todo.db?check_same_thread=False')
 
-# creating declarative meta class from which our Tasks class will inherite
+# creating declarative meta class from which our Tasks class will inherit
 Base = declarative_base()
 
 
@@ -118,9 +118,10 @@ class Task(Base):
     id = Column(Integer, primary_key=True)
     task = Column(String, default='')
     deadline = Column(Date, default=datetime.today)
-    
+
     def __repr__(self):
         return self.task
+
 
 # create a table in our DB
 Base.metadata.create_all(engine)
@@ -133,45 +134,97 @@ class TaskList:
         self.Session = sessionmaker(bind=engine)
         self.session = self.Session()
         self.menu()
-    
+
     # main menu and handling user's choices
     def menu(self):
         choice = input('''1) Today's tasks
-2) Add task
+2) Week's tasks
+3) All tasks
+4) Add task
 0) Exit\n''')
-        if choice == '1':
+        if choice == '1':  # Today's tasks
             self.today_tasks()
-        elif choice == '2':
+        elif choice == '2':  # Week's tasks
+            self.week_tasks()
+        elif choice == '3':  # All tasks
+            self.all_tasks()
+        elif choice == '4':  # Add task
             self.add_task()
-        elif choice == '0':
+        elif choice == '0':  # Exit
             self.exit()
-    
+
     # printing tasks from database
     def today_tasks(self):
         rows = self.session.query(Task).all()
-        print('\nToday:')
+        today = datetime.today()
+        print('\nToday', today.day, today.strftime('%b') + ':')
         if len(rows) == 0:
+            print('Nothing to do!\n')
+            return self.menu()
+        i = 1
+        for row in rows:
+            if row.deadline == today.date():
+                print(f'{i}. {row.task}')
+                i += 1
+        if i == 1:
+            print('Nothing to do!\n')
+        print('')
+        self.menu()
+
+    # printing all tasks for the next 7 days from today.
+    def week_tasks(self):
+        today = datetime.today()
+        # filtering tasks (today + 7 days)
+        rows = self.session.query(Task).filter(and_(Task.deadline <= today.date() + timedelta(days=6), Task.deadline >= today.date())).order_by(Task.deadline).all()
+        # creating an auxiliary lists with the next 7 days and days where we have tasks
+        all_days_lst = [int(today.strftime('%d')) + _ for _ in range(7)]
+        task_days_lst = [row.deadline.day for row in rows]
+        # printing the output formatted as asked by the task
+        print('')
+        for i in range(7):
+            today = datetime.today() + timedelta(days=i)
+            print(today.strftime('%A %d %b') + ':')
+            if int(today.strftime('%d')) in task_days_lst:  # checking if we have a task on this day
+                i = 1
+                for row in rows:
+                    if row.deadline.day == int(today.strftime('%d')):
+                        print(f'{i}. {row.task}')
+                        i += 1
+                print('')
+            else:
+                print('Nothing to do!\n')
+        self.menu()
+
+    # printing all tasks from database
+    def all_tasks(self):
+        rows = self.session.query(Task).order_by(Task.deadline).all()
+        if len(rows) == 0:
+            print('\nAll tasks:')
             print('Nothing to do!\n')
             self.menu()
         else:
+            print('\nAll tasks:')
+            i = 1
             for row in rows:
-                print(f'{row.id}. {row.task}')
+                print(f'{i}. {row.task}. {row.deadline.day}', row.deadline.strftime('%b'))
+                i += 1
             print('')
             self.menu()
-    
+
     # adding a task to database
     def add_task(self):
         task_to_add = input('\nEnter task\n')
-        new_task = Task(task=task_to_add,)
+        task_deadline = input('Enter deadline\n')
+        new_task = Task(task=task_to_add, deadline=datetime.strptime(task_deadline, '%Y-%m-%d').date(),)
         self.session.add(new_task)
         self.session.commit()
         print('The task has been added!\n')
         self.menu()
-    
+
     # exiting the script
     def exit(self):
         print('\nBye!')
         return
-       
+
 
 my_day = TaskList()
