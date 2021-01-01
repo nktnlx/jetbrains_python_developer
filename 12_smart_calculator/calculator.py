dd91293c4656b1f6c352c5113cc9a7a37209dc15
nktnlx <nktn.lx@gmail.com>
2021-01-01 15:46:15 +0300

    calculator stage 1

--- 12_smart_calculator/calculator.py
+++ 12_smart_calculator/calculator.py
@@ -1,1 +1,11 @@
 # STAGE 1
+class Calculator:
+    def __init__(self, nums):
+        self.nums = nums
+
+    def addition(self):
+        return self.nums[0] + self.nums[1]
+
+
+my_calc = Calculator([int(x) for x in input().split()])
+print(my_calc.addition())
